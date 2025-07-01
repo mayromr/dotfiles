ff715afa5bf0d677708063cea8aa04e2568c6586
nhruo123 <--global>
2025-07-01 13:40:45 +0300

    feat: range formatting

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
+      mode = { 'n', 'v' },
       desc = 'Format buffer',
     },
   },

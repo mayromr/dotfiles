e37e4ade5d803afa6f8c1fcd45132dfdadb42395
nhruo123 <--global>
2025-09-07 13:40:21 +0300

    chore: install nvm and pyenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,5 @@
-# Stolen from Folke repo :)
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    # Cursor styles
-    set -gx fish_vi_force_cursor 1
-    set -gx fish_cursor_default block
-    set -gx fish_cursor_insert line blink
-    set -gx fish_cursor_visual block
-    set -gx fish_cursor_replace_one underscore
 
     # Path
     set -x fish_user_paths
@@ -31,4 +23,5 @@ if status is-interactive
     abbr v nvim
 
     starship init fish | source
+    pyenv init - fish | source
 end

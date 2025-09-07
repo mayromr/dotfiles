50e7773d517461e5f5ad54d152f09b2efbb28526
nhruo123 <--global>
2025-09-07 18:02:24 +0300

    feat: fish and stuff

--- fish/.config/fish/functions/venv.fish
+++ fish/.config/fish/functions/venv.fish
@@ -1,6 +1,9 @@
 function venv
     if test "$VENV_DIR"
-        source "$VENV_DIR$(command ls $VENV_DIR | fzf)/bin/activate.fish"
+        set -l selected_venv (command ls $VENV_DIR | fzf)
+        if test -n "$status"
+            source "$VENV_DIR$selected_venv/bin/activate.fish"
+        end
     else
         echo "Please set your venv dir using `set -Ux VENV_DIR <VENV_DIR_PATH>`"
     end

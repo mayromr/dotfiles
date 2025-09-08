99a4acc7590c6a2b5973790b4a35498107af7195
nhruo123 <--global>
2025-09-08 16:35:53 +0300

    fix: broken aliases and alias cat to bat

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,9 +13,11 @@ if status is-interactive
     # Exports
     set -x MANPAGER "nvim +Man!"
 
-    alias ls="eza --color=always --icons --group-directories-first"
-    alias la 'eza --color=always --icons --group-directories-first --all'
-    alias ll 'eza --color=always --icons --group-directories-first --all --long'
+    alias ls="eza --icons --group-directories-first"
+    alias la 'eza --icons --group-directories-first --all'
+    alias ll 'eza --icons --group-directories-first --all --long'
+    alias cat bat
+
     abbr l ll
 
     # Editor

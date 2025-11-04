77aa0656b07dab4a170a66b618009a98850aad53
Mayrom Rabinovich <nhruo123@gmail.com>
2025-11-04 22:16:00 +0200

    feat; codebook testing env

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-STOW_FOLDERS="nvim,tmux,zsh,alacritty,bottom,karabiner,fish,starship"
+STOW_FOLDERS="nvim,tmux,zsh,alacritty,bottom,karabiner,fish,starship,codebook"
 DOTFILES=$HOME/dotfiles
 
 pushd $DOTFILES
